105687e5c7619a1a00a7df473e605cd13f93c972
50055699 <50055699@serc.ac.uk>
2024-05-08 14:37:53 +0100

    Initial commit

--- src/Kettle.java
+++ src/Kettle.java
@@ -4,5 +4,58 @@ public class Kettle {
     private int amountOfWater;
     private int waterTemperature;
 
+    public Kettle(int capacity) {
 
+        this.capacity = capacity;
+        this.amountOfWater = 0;
+        this.waterTemperature = 0;
+
+    }
+
+
+
+
+    public void addWater(int amountAdd){
+
+        amountOfWater += amountAdd;
+
+    }
+
+    public void dispenseWater(int amountRemove) {
+
+        amountOfWater -= amountRemove;
+
+    }
+
+    public void heat() {
+
+        if (waterTemperature < 100){
+
+            waterTemperature ++;
+        }
+
+        return waterTemperature;
+
+        }
+
+        public void cool() {
+
+        if (waterTemperature > 20){
+
+            waterTemperature --;
+
+        }
+
+        public boolean isBoiled() {
+
+            return waterTemperature > 92;
+
+            }
+
+
+    }
 }
+
+
+
+
